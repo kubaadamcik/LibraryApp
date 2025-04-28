a713daea073107b7ac0abd323fb0d3a622e04873
kubaadamcik <jakubadamcik11@gmail.com>
2025-04-28 12:13:29 +0200

    Made Book

--- src/Library.Domain/Entities/Book.cs
+++ src/Library.Domain/Entities/Book.cs
@@ -2,5 +2,16 @@ namespace Library.Domain.Entities;
 
 public class Book
 {
-    // TODO: Complete the class
+    public string Title { get; set; }
+    public string Author { get; set; }
+    public string Description { get; set; }
+    public int Pages { get; set; }
+    public double Price { get; set; }
+
+    public Book(string title, string author, int pages)
+    {
+        Title = title;
+        Author = author;
+        Pages = pages;
+    }
 }
