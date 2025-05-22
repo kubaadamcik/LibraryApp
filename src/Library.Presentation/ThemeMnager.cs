0a193055d2dcf7aba567984f2a88106a92d2d9d5
lamaxik <bedravamatej@gmail.com>
2025-05-22 22:39:17 +0200

    update

--- src/Library.Presentation/ThemeMnager.cs
+++ src/Library.Presentation/ThemeMnager.cs
@@ -12,23 +12,26 @@ namespace ZaverecnyProjekt
         public bool IsDarkTheme { get; private set; }
 
         private readonly SolidColorBrush _darkBackground = new(Color.FromRgb(87, 87, 87));
-        private readonly SolidColorBrush _lightBackground = new(Color.FromRgb(219, 219, 219));
+        private readonly SolidColorBrush _lightBackground = new(Color.FromRgb(235, 235, 235));
         private readonly SolidColorBrush _darkForeground = new(Color.FromRgb(255, 255, 255));
         private readonly SolidColorBrush _lightForeground = new(Color.FromRgb(0, 0, 0));
         private readonly SolidColorBrush _lightbuttonBackground = new(Color.FromRgb(217, 217, 217));
         private readonly SolidColorBrush _darkbuttonBackground = new(Color.FromRgb(133, 133, 133));
         private readonly SolidColorBrush _darkbuttonForeground = new(Color.FromRgb(255, 255, 255));
         private readonly SolidColorBrush _lightbuttonForeground = new(Color.FromRgb(0, 0, 0));
-        
+        private readonly SolidColorBrush _lightTrigger = new(Color.FromRgb(196, 196, 196));
+        private readonly SolidColorBrush _darkTrigger = new(Color.FromRgb(69, 69, 69));
+
         public void SetTheme(bool isDark)
         {
             IsDarkTheme = isDark;
-    
+
             var resources = Application.Current.Resources;
             resources["BackgroundBrush"] = isDark ? _darkBackground : _lightBackground;
             resources["ForegroundBrush"] = isDark ? _darkForeground : _lightForeground;
             resources["ButtonBackgroundBrush"] = isDark ? _darkbuttonBackground : _lightbuttonBackground;
             resources["ButtonForegroundBrush"] = isDark ? _darkbuttonForeground : _lightbuttonForeground;
-        }
+            resources["ButtonTriggerBrush"] = isDark ? _darkTrigger : _lightTrigger;
+    }
     }
 }
