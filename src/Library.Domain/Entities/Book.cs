f30f8bbf16e01f7c6e14caf450742024c100f7da
Elexic <89549464+elex1c@users.noreply.github.com>
2025-05-26 11:11:07 +0200

    ReaderInfo service deleted

--- src/Library.Domain/Entities/Book.cs
+++ src/Library.Domain/Entities/Book.cs
@@ -1,10 +1,19 @@
+using System.ComponentModel.DataAnnotations;
+
 namespace Library.Domain.Entities;
 
 public class Book
 {
     public int Id { get; set; }
-    public string Title { get; set; } = string.Empty;
-    public string Author { get; set; } = string.Empty;
+    [MaxLength(100)]
+    [Required]
+    public required string Title { get; set; }
+    [Required]
+    [MaxLength(100)]
+    public required string Author { get; set; }
+    [Required]
+    [MaxLength(250)]
+    public required string Description { get; set; }
     public int Count { get; set; }
     public int Pages { get; set; }
     public int Year { get; set; }
