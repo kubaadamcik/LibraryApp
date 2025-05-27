0e69290811c54adf85eed2b3e07aa26aeacd6cda
59286bedrava <59286bedrava@V0813>
2025-05-27 10:31:09 +0200

    a,jsbdakjsbgf

--- src/Library.Presentation/View/Pages/BorrowBook.xaml.cs
+++ src/Library.Presentation/View/Pages/BorrowBook.xaml.cs
@@ -1,6 +1,7 @@
 ﻿using System.Collections.ObjectModel;
 using System.ComponentModel;
 using System.Runtime.CompilerServices;
+using System.Threading.Tasks;
 using System.Windows;
 using System.Windows.Controls;
 using Library.Application.Interfaces;
@@ -11,43 +12,50 @@ namespace ZaverecnyProjekt.View.Pages;
 
 public partial class BorrowBook : Page, INotifyPropertyChanged
 {
-    private IBookService _bookService { get; set; }
-    private IReaderService _readerService { get; set; }
-    private ILibraryService _libraryService { get; set; }
+    private IBookService _bookService;
+    private IReaderService _readerService;
+    private ILibraryService _libraryService;
+
     private ObservableCollection<Book> _books;
+    private ObservableCollection<Reader> _readers;
 
     public ObservableCollection<Book> Books
     {
         get => _books;
-        set
-        {
-            _books = value;
-            OnPropertyChanged(nameof(Books));
-        }
+        set { _books = value; OnPropertyChanged(); }
+    }
+
+    public ObservableCollection<Reader> Readers
+    {
+        get => _readers;
+        set { _readers = value; OnPropertyChanged(); }
     }
 
     public BorrowBook(IBookService bookService, IReaderService readerService, ILibraryService libraryService)
     {
         InitializeComponent();
-
         _bookService = bookService;
         _readerService = readerService;
         _libraryService = libraryService;
 
+        DataContext = this;       // <- PŘESUNUTO SEM
+
         Loaded += OnLoaded;
     }
 
-    private void OnLoaded(object sender, RoutedEventArgs e)
+    private async void OnLoaded(object sender, RoutedEventArgs e)
     {
-        GetAllBooks();
-        DataContext = this;
+        await LoadData();            // <-- chybělo
+        DataContext = this;          // <-- chybělo
     }
 
-    public async Task GetAllBooks()
+    private async Task LoadData()
     {
-        var _books = await _bookService.GetAllBooks();
+        var bookList = await _bookService.GetAllBooks();
+        Books = new ObservableCollection<Book>(bookList);
 
-        Books = new ObservableCollection<Book>(_books);
+        var readerList = await _readerService.GetAllReaders();
+        Readers = new ObservableCollection<Reader>(readerList);
     }
 
     private void RemoveSearchPlaceholder(object sender, RoutedEventArgs e)
@@ -62,26 +70,27 @@ public partial class BorrowBook : Page, INotifyPropertyChanged
 
     private void SelectReader(object sender, RoutedEventArgs e)
     {
-        int bookId = _books[DgBooks.SelectedIndex].Id;
-        SelectReaderDialog dialog = new SelectReaderDialog(_readerService, _libraryService, bookId, _bookService);
+        if (DgBooks.SelectedIndex == -1 || DgReaders.SelectedIndex == -1) return;
 
+        var selectedBook = Books[DgBooks.SelectedIndex];
+        var dialog = new SelectReaderDialog(_readerService, _libraryService, selectedBook.Id, _bookService);
         dialog.ShowDialog();
     }
 
-    private void CreateBook(object sender, RoutedEventArgs e)
+    private async void CreateBook(object sender, RoutedEventArgs e)
     {
-        _bookService.AddBook(new Book()
+        await _bookService.AddBook(new Book()
         {
-            Author = "Jakub Adamčík", Description = "It's an interesting book!", Pages = 69, Title = tb_searchBox.Text
+            Author = "Jakub Adamčík",
+            Description = "It's an interesting book!",
+            Pages = 69,
+            Title = tb_searchBox.Text
         });
 
-        GetAllBooks();
+        await LoadData();
     }
 
     public event PropertyChangedEventHandler PropertyChanged;
-
-    protected void OnPropertyChanged(string propertyName)
-    {
+    protected void OnPropertyChanged([CallerMemberName] string propertyName = "") =>
         PropertyChanged?.Invoke(this, new PropertyChangedEventArgs(propertyName));
-    }
 }
